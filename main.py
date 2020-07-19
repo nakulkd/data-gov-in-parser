20306e16384baa369d9e3f3083435ebe27eeeedf
Nakul Kishore Dumblekar <nakul.dumblekar@gmail.com>
2020-07-20 01:47:30 +0530

    added comments for readability

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ path = input('Enter the API GET URL (ex: /resource/...): ')
 inp_format = input('Enter the format of data (default is JSON): ')
 lim = input('Enter the entry size (default is 10, leave blank for all): ')
 
-if len(lim) == 0:
+if len(lim) == 0: # This is to speed up the query process.
     query = DGS(path, get_format=inp_format)
 else:
     query = DGS(path, get_format=inp_format, limit=lim)
@@ -13,12 +13,12 @@ else:
 print('Data has been queried. Converting to csv...')
 
 decoded_content = query.json()
-decoded_records = decoded_content['records']
+decoded_records = decoded_content['records'] # hard-coded based on observed output
 
-filename = decoded_content['title'] + '.csv'
+filename = decoded_content['title'] + '.csv' # output file will be named the same as the dataset
 keys = decoded_records[0].keys()
 
-with open(filename, mode='w', newline='') as file:
+with open(filename, mode='w', newline='') as file: # currently only works for JSON output and WILL THROW AN ERROR for all other types
     dict_writer = csv.DictWriter(file, keys)
     dict_writer.writeheader()
     dict_writer.writerows(decoded_records)
